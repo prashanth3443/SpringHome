9107244892f291afe14e92838a36916ba440fadd
Prashanth <prashanthcherry001@gmail.com>
2022-05-14 16:20:31 +0530

    SpringApplication

---
+++ src/main/java/com/springboote/demon/DemonApplication.java
@@ -0,0 +1,14 @@
+package com.springboote.demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DemonApplication.class, args);
+	}
+
+}
